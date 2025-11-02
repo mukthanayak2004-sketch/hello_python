cfbf6ee90c3a08b2e451075ece899105327aabd5
mukthanayak2004-sketch <muktha.nayak2004@gmail.com>
2025-11-02 19:09:18 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,8 @@
+# hello.py
+name = input("Enter your name: ")
+print("Hello,", name)
+
+# simple add
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+print("Sum:", a + b)
